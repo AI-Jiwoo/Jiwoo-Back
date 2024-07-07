7c5e04840ccb41f86343c10978817697f32132d8
moomint8 <office05053@gmail.com>
2024-07-07 22:28:26 +0900

    build(main): 프로젝트 초기 세팅
    - build.gradle 기본적인 라이브러리 추가
    - .gitIgnore 기본적인 설정

---
+++ src/main/java/org/jiwoo/miniback/MiniBackApplication.java
@@ -0,0 +1,13 @@
+package org.jiwoo.miniback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/jiwoo/miniback/MiniBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jiwoo.miniback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
