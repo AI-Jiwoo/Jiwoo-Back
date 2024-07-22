0adc6d2e5fbe6ea819514dbe58e3891b92e830cb
moomint8 <office05053@gmail.com>
2024-07-23 01:21:33 +0900

    feat(OpenAI API): OpenAI API 연동 공통 모듈 구현
    OpenAI API와 연동하는 공통 모듈을 만들었습니다.
    String으로 요청보낼 Prompt를 보내면 응답을 String으로 반환합니다.

---
+++ src/main/java/org/jiwoo/back/common/OpenAI/service/OpenAIServiceImpl.java
@@ -0,0 +1,43 @@
+package org.jiwoo.back.common.OpenAI.service;
+
+import org.jiwoo.back.common.OpenAI.dto.OpenAIRequestDTO;
+import org.jiwoo.back.common.OpenAI.dto.OpenAIResponseDTO;
+import org.jiwoo.back.common.exception.OpenAIResponseFailException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class OpenAIServiceImpl implements OpenAIService {
+
+    private final Map<String, String> openAIEnv = new HashMap<>();
+
+    private final RestTemplate template;
+
+    @Autowired
+    public OpenAIServiceImpl(Environment env, RestTemplate template) {
+        openAIEnv.put("model", env.getProperty("open-ai.model"));
+        openAIEnv.put("url", env.getProperty("open-ai.url"));
+        this.template = template;
+    }
+
+    @Override
+    public String generateAnswer(String questionPrompt) throws OpenAIResponseFailException {
+
+        String model = openAIEnv.get("model");
+        String apiURL = openAIEnv.get("url");
+
+        OpenAIRequestDTO openAIRequest = new OpenAIRequestDTO(model, questionPrompt);
+        OpenAIResponseDTO openAIResponse = template.postForObject(apiURL, openAIRequest, OpenAIResponseDTO.class);
+
+        if (openAIResponse == null) {
+            throw new OpenAIResponseFailException();
+        }
+
+        return openAIResponse.choices().get(0).message().content();
+    }
+}
