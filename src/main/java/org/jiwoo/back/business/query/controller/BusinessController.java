a2fa16bd57b9068d83c32edb66e281070a21fd17
Leegiyeon <rldus3512@naver.com>
2024-07-28 13:47:48 +0900

    feat(market_research): 사업 ID로 사업 내용 조회 Controller 생성

---
+++ src/main/java/org/jiwoo/back/business/query/controller/BusinessController.java
@@ -0,0 +1,33 @@
+package org.jiwoo.back.business.query.controller;
+
+import org.jiwoo.back.business.query.aggregate.vo.ResponseBusinessVO;
+import org.jiwoo.back.business.query.dto.BusinessDTO;
+import org.jiwoo.back.business.query.service.BusinessService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/business")
+public class BusinessController {
+    private final BusinessService businessService;
+
+    @Autowired
+    public BusinessController(BusinessService businessService) {
+        this.businessService = businessService;
+    }
+
+    /* 설명. 사업 내용 조회 */
+    @GetMapping("/id/{id}")
+    public ResponseEntity<ResponseBusinessVO> getBusinessById(@PathVariable("id") int id) {
+        BusinessDTO business = businessService.findBusinessById(id);
+        return ResponseEntity.status(HttpStatus.OK).body(new ResponseBusinessVO("조회 성공", List.of(business)));
+    }
+
+}
