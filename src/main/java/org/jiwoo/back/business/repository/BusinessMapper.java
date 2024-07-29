70a3bea76f3883be7060564588b8a1a4046e189a
Leegiyeon <rldus3512@naver.com>
2024-07-29 15:08:04 +0900

    refactor(market_research): Business 기능 JPA로 전환하기 위한 Service, Repository 수정

--- src/main/java/org/jiwoo/back/business/repository/BusinessMapper.java
+++
@@ -1,9 +0,0 @@
-package org.jiwoo.back.business.repository;
-
-import org.apache.ibatis.annotations.Mapper;
-import org.jiwoo.back.business.aggregate.entity.Business;
-
-@Mapper
-public interface BusinessMapper {
-    Business findById(int id);
-}
