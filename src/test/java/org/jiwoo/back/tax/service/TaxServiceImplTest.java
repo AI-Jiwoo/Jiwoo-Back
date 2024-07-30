9b2799c4e2e45ffe2f0c8f2e6a0d64096ad10171
lja9207 <3wksu2@hanmail.net>
2024-07-30 11:38:33 +0900

    feat#1 tax_treatment test file making

---
+++ src/test/java/org/jiwoo/back/tax/service/TaxServiceImplTest.java
@@ -0,0 +1,19 @@
+package org.jiwoo.back.tax.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TaxServiceImplTest {
+
+
+    @Test
+    void testTaxService() {
+        //given
+        //when
+        //then
+
+
+    }
+
+}
