138a1bd37f3f3f7e7ba659f1a2a5dfdb3c19c2d9
moomint8 <office05053@gmail.com>
2024-08-20 00:34:34 +0900

    test(SupportProgram): 지원 사업 입력 시 관련 사업 반환 테스트 구현
    지원 사업 입력 시 관련 사업 반환 테스트 구현

---
+++ src/main/java/org/jiwoo/back/supprotProgram/service/SupportProgramService.java
@@ -0,0 +1,9 @@
+package org.jiwoo.back.supprotProgram.service;
+
+import org.jiwoo.back.supprotProgram.aggregate.dto.SupportProgramDTO;
+
+import java.util.List;
+
+public interface SupportProgramService {
+    void insertSupportProgram(List<SupportProgramDTO> supportProgramDTO);
+}
