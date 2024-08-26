40ca6e80ff9c48c88732e4c124a94401010e4647
lja9207 <3wksu2@hanmail.net>
2024-08-26 13:17:38 +0900

    [fix]응답VO 생성

--- src/main/java/org/jiwoo/back/taxation/controller/TaxationController.java
+++ src/main/java/org/jiwoo/back/taxation/controller/TaxationController.java
@@ -1,8 +1,12 @@
 package org.jiwoo.back.taxation.controller;
 
+import org.jiwoo.back.business.aggregate.vo.ResponseBusinessVO;
+import org.jiwoo.back.taxation.aggregate.vo.ResponseTaxationVO;
 import org.jiwoo.back.taxation.dto.TaxationDTO;
 import org.jiwoo.back.taxation.service.HomeTaxAPIService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,14 +23,15 @@ public class TaxationController {
 
     }
 
-    @PostMapping("/")
-    public ResponseEntity<TaxationDTO> getTaxation(@RequestParam("businessId") int businessId) {
+    @PostMapping(value = "/homeTax", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public ResponseEntity<ResponseTaxationVO> getTaxation(@RequestParam("businessId") int businessId) {
 
         try{
             TaxationDTO taxationDTO = homeTaxAPIService.getTaxationInfo(businessId);
-            return ResponseEntity.ok(taxationDTO);
+            return ResponseEntity.ok(new ResponseTaxationVO(HttpStatus.OK, "홈택스API 조회가 완료되었습니다.", taxationDTO));
         }catch(Exception e){
-            return ResponseEntity.status(500).body(null);
+            return ResponseEntity.badRequest().body(new ResponseTaxationVO(HttpStatus.BAD_REQUEST, "홈택스 API 조회 실패 :" + e.getMessage(), null));
         }
 
     }
